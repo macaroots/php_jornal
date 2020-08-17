952df6bb36aa9978d1e455f9e48476b6bf8999a6
macaroots <58048446+macaroots@users.noreply.github.com>
2020-08-17 15:14:27 -0300

    Update DAO_Abstrato.php

--- lib/DAO/DAO_Abstrato.php
+++ lib/DAO/DAO_Abstrato.php
@@ -2,6 +2,7 @@
 class DAO_Abstrato {
 	function conecta() {
 		$conexao = new PDO('mysql:host=localhost;dbname=renato', 'renato', '123');
+		$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $conexao;
 	}
 }
